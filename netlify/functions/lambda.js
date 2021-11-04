79aa289825bc800de54d1e71d4eb5e281e69271b
sunbrogilgamesh <87723910+sunbrogilgamesh@users.noreply.github.com>
2021-11-04 21:58:04 +0100

    update

--- netlify/functions/lambda.js
+++ netlify/functions/lambda.js
@@ -1,7 +1,7 @@
 exports.handler = async event => {
 
   const API_KEY =  process.env.API_KEY
-  const fetch = require('node-fetch')
+  import fetch from "node-fetch";
   const cityQuery = "Milan"
   const response = await fetch("https://api.waqi.info/feed/" + cityQuery + "/?token=" + API_KEY)
   const data = await response.json()
