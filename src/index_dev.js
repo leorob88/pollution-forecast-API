a730df3ab0bbe7c588d4bb6a91ee8c923c9a8485
sunbrogilgamesh <87723910+sunbrogilgamesh@users.noreply.github.com>
2021-11-09 12:33:13 +0100

    update

--- src/index_dev.js
+++ src/index_dev.js
@@ -4,6 +4,25 @@ async function requestPollutionData() {
 
   const API_KEY = process.env.API_KEY;
 
+  function infos(pos) {
+    var crd = pos.coords;
+    //console.log(pos);
+    console.log('Current position:');
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`(Accuracy: about ${crd.accuracy} meters.`);
+  }
+  function error(err) {
+    console.warn(`Error(${err.code}): ${err.message}`);
+  }
+  var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+
+  navigator.geolocation.getCurrentPosition(infos, error, options);
+
   var cityQuery = "Milan";
   const response = await fetch("https://api.waqi.info/feed/" + cityQuery + "/?token=" + API_KEY);
   const data = await response.json();
