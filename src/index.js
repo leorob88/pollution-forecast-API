6e055bc1f646614b7d0a8772dd8d64a0f0205d5d
sunbrogilgamesh <87723910+sunbrogilgamesh@users.noreply.github.com>
2021-11-16 16:44:55 +0100

    update

--- src/index.js
+++ src/index.js
@@ -7,22 +7,7 @@ async function callLambdaFunction() {
     document.getElementById("answer").innerHTML = "The location you searched for has no stations for pollution detection.";
   }
   else{
-    var aqi = data.data.aqi;
-    var more;
-    if (aqi > 300){
-      more = "hazardous";
-    }else if (aqi > 200) {
-      more = "very unhealthy";
-    }else if (aqi > 150) {
-      more = "unhealthy"
-    }else if (aqi > 100) {
-      more = "unhealthy for sensitive groups"
-    }else if (aqi > 50) {
-      more = "moderate"
-    }else {
-      more = "good"
-    }
-    document.getElementById("answer").innerHTML = `The estimated AQI for ${data.data.city.name} has a value of ${aqi}. The pollution rate is ${more}.`;
+    document.getElementById("answer").innerHTML = `The estimated AQI for ${data.data.city.name} has a value of ${data.data.aqi}. The pollution rate is .`;
     // const near = await fetch("https://api.waqi.info/feed/geo:" + data.data.city.geo[0] + ";" + data.data.city.geo[1] + "/?token=" + API_KEY);
     // const station = await near.json();
     // document.getElementById("answer").innerHTML = `The nearest station to your estimated position is in ${station.data.city.name}. The estimated AQI has a value of ${station.data.aqi}. The pollution rate is ${more}.`;
