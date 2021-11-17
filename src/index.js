d8bcfb4b63774137e4e5506524687868e3a169b4
sunbrogilgamesh <87723910+sunbrogilgamesh@users.noreply.github.com>
2021-11-17 15:44:46 +0100

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ async function callLambdaFunction() {
   var cityQuery = /*document.getElementById("query").value*/ "London";
   const response = await fetch("/.netlify/functions/lambda?name=${cityQuery}");
   const data = await response.json();
-
+  console.log(data);
   if (data.data == "Unknown station"){
     console.log/*document.getElementById("answer").innerHTML = */("The location you searched for has no stations for pollution detection.");
   }
