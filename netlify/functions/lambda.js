d86fea165fd8091987ba75dd7f181e38d5ae8a83
sunbrogilgamesh <87723910+sunbrogilgamesh@users.noreply.github.com>
2021-11-18 16:07:30 +0100

    update

--- netlify/functions/lambda.js
+++ netlify/functions/lambda.js
@@ -5,7 +5,7 @@ exports.handler = async event => {
   const API_KEY =  process.env.API_KEY;
 
   var cityQuery = event.queryStringParameters.name;
-  const response = await fetch("https://api.waqi.info/feed/" + cityQuery /*"here"*/ + "/?token=" + API_KEY);
+  const response = await fetch("https://api.waqi.info/feed/" + cityQuery + "/?token=" + API_KEY);
   const data = await response.json();
 
   console.log(data);
