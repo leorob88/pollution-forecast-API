8322a059dd63d7b14a1dba4d141a012ea332ebf8
sunbrogilgamesh <87723910+sunbrogilgamesh@users.noreply.github.com>
2021-11-18 19:14:27 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,28 +1,10 @@
 
-var crd;
-function infos(pos) {
-  crd = pos.coords;
-  console.log(pos);
-  console.log('Current position:');
-  console.log(`Latitude : ${crd.latitude}`);
-  console.log(`Longitude: ${crd.longitude}`);
-  console.log(`(Accuracy: about ${crd.accuracy} meters.`);
-}
-function error(err) {
-  console.warn(`Error(${err.code}): ${err.message}`);
-}
-var options = {
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 0
-};
 
-navigator.geolocation.getCurrentPosition(infos, error, options);
 
 var check = 0;
-var queryarg = document.getElementById("query").value /*`geo:${crd.latitude};${crd.longitude}`*/;
+var queryarg = document.getElementById("query").value/*`geo:${crd.latitude};${crd.longitude}`*/;
 
-async function callLambdaFunction() {
+async function callLambdaFunction(queryarg) {
 
   const response = await fetch(`/.netlify/functions/lambda?name=${queryarg}`);
   const data = await response.json();
@@ -30,14 +12,32 @@ async function callLambdaFunction() {
   console.log(data);
 
   if (data.data == "Unknown station"){
-    // if (check == 0){
-      document.getElementById("answer").innerHTML = "The location you searched for has no stations for pollution detection.";
-    //   queryarg = `geo:${crd.latitude};${crd.longitude}`;
-    //   check = 1;
-    //   callLambdaFunction(queryarg);
-    // }else{
-    //   document.getElementById("answer").innerHTML += " I also tried to locate your position but still it has no stations for pollution detection.";
-    // }
+    if (check == 0){
+      document.getElementById("answer").innerHTML = "I couldn't find any stations for pollution detection in the location you searched for.";
+      function infos(pos) {
+        var crd = pos.coords;
+        console.log(pos);
+        console.log('Current position:');
+        console.log(`Latitude : ${crd.latitude}`);
+        console.log(`Longitude: ${crd.longitude}`);
+        console.log(`(Accuracy: about ${crd.accuracy} meters.`);
+      }
+      function error(err) {
+        console.warn(`Error(${err.code}): ${err.message}`);
+      }
+      var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      };
+
+      navigator.geolocation.getCurrentPosition(infos, error, options);
+      queryarg = `geo:${crd.latitude};${crd.longitude}`;
+      check = 1;
+      callLambdaFunction(queryarg);
+    }else{
+      document.getElementById("answer").innerHTML += " I also tried to locate your position but still it has no stations for pollution detection.";
+    }
   }
   else{
     var aqi = data.data.aqi;
@@ -61,4 +61,4 @@ async function callLambdaFunction() {
   }
 }
 
-callLambdaFunction();
+callLambdaFunction(queryarg);
