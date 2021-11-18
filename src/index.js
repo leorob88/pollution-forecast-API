d6c33861bde8c767e1194f3755ca606048fb2acd
sunbrogilgamesh <87723910+sunbrogilgamesh@users.noreply.github.com>
2021-11-18 15:57:14 +0100

    update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ async function callLambdaFunction() {
   const response = await fetch(`/.netlify/functions/lambda?name=${cityQuery}`);
   const data = await response.json();
   if (data.data == "Unknown station"){
-    console.log/*document.getElementById("answer").innerHTML = */("The location you searched for has no stations for pollution detection.");
+    document.getElementById("answer").innerHTML = "The location you searched for has no stations for pollution detection.";
   }
   else{
     var aqi = data.data.aqi;
@@ -23,11 +23,11 @@ async function callLambdaFunction() {
     }else {
       more = "good";
     }
-    console.log/*document.getElementById("answer").innerHTML = */(`The estimated AQI for ${data.data.city.name} has a value of ${aqi}. The pollution rate is ${more}.`);
+    document.getElementById("answer").innerHTML = `The estimated AQI for ${data.data.city.name} has a value of ${aqi}. The pollution rate is ${more}.`;
     // const near = await fetch("https://api.waqi.info/feed/geo:" + data.data.city.geo[0] + ";" + data.data.city.geo[1] + "/?token=" + API_KEY);
     // const station = await near.json();
     // document.getElementById("answer").innerHTML = `The nearest station to your estimated position is in ${station.data.city.name}. The estimated AQI has a value of ${station.data.aqi}. The pollution rate is ${more}.`;
-    console.log/*document.getElementById("answer").innerHTML += */(` For further details, you can check out the reference website infos <a href="https://www.airnow.gov/aqi/aqi-basics/">here</a>.`);
+    document.getElementById("answer").innerHTML += ` For further details, you can check out the reference website infos <a href="https://www.airnow.gov/aqi/aqi-basics/">here</a>.`;
   }
 }
 
