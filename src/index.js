4584db57aa18dd5ace87399ec55573e3840cb7dc
sunbrogilgamesh <87723910+sunbrogilgamesh@users.noreply.github.com>
2021-11-22 15:26:47 +0100

    update

--- src/index.js
+++ src/index.js
@@ -12,14 +12,6 @@ function locate(){
   let x = 10;
 }
 
-    var butt1 = document.createElement("button");
-    butt1.id = "button1";
-    butt1.innerHTML = "input";
-    document.body.appendChild(butt1);
-    var butt2 = document.createElement("button");
-    butt2.id = "button2";
-    butt2.innerHTML = "geo";
-    document.body.appendChild(butt2);
     document.getElementById("butt1").addEventListener("click", function(){
       select(document.getElementById("query").value);
     });
