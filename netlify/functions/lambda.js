716e1a8cdb0057f453966c855b9fa9cd764ad9cf
sunbrogilgamesh <87723910+sunbrogilgamesh@users.noreply.github.com>
2021-11-22 17:08:16 +0100

    update

--- netlify/functions/lambda.js
+++ netlify/functions/lambda.js
@@ -1,19 +1,21 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const API_KEY =  process.env.API_KEY;
 
 exports.handler = async event => {
 
-  const API_KEY =  process.env.API_KEY;
+  let target = event.queryStringParameters;
+  let data;
 
-  let city = event.queryStringParameters.name;
-  const response = await fetch("https://api.waqi.info/feed/" + city + "/?token=" + API_KEY);
-  const data = await response.json();
+  if (target.city){
+    const response = await fetch(`https://api.waqi.info/feed/${target.city}/?token=${API_KEY}`);
+    data = await response.json();
+  }else if (target.lat && target.lon) {
+    const response = await fetch(`https://api.waqi.info/feed/geo:${target.lat};${target.lon}/?token=${API_KEY}`);
+    data = await response.json();
+  }
 
-  const pass = (body) => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify(body)
-    }
-  };
-
-  return pass(data);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data)
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,83 @@
 
-
-async function select(city){
-
-  const response = await fetch(`/.netlify/functions/lambda?name=${city}`);
-  const data = await response.json();
-
-  console.log(data);
+//main function for fetch
+function locating(location){
+  //fetch infos with given input via buttons
+  fetch(`/.netlify/functions/lambda?${location}`)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    //if response is not found,
+    if (data.data == "Unknown station"){
+      //if search by text
+      if (location.indexOf(":") == -1){
+        //tell user input text result was not found
+        document.getElementById("answer").innerHTML = "I couldn't find any stations for pollution detection in the location you searched for.";
+      }
+      //if search by geoloc
+      else {
+        //tell user geolocation result was not found
+        document.getElementById("answer").innerHTML = "I couldn't find any stations for pollution detection with your current position.";
+      }
+    }
+    //if response is found
+    else {
+      //take notice of aqi value for further text info
+      let aqi = data.data.aqi;
+      let more;
+      if (aqi > 300){
+        more = "hazardous";
+      }else if (aqi > 200) {
+        more = "very unhealthy";
+      }else if (aqi > 150) {
+        more = "unhealthy";
+      }else if (aqi > 100) {
+        more = "unhealthy for sensitive groups";
+      }else if (aqi > 50) {
+        more = "moderate";
+      }else {
+        more = "good";
+      }
+      //if search by text
+      if (location.indexOf(":") == -1){
+        //tell user the result and quality for their searched position
+        document.getElementById("answer").innerHTML = `The estimated AQI for ${data.data.city.name} has a value of ${aqi}. The pollution rate is ${more}.`;
+      }
+      //if search by geoloc
+      else {
+        //tell user the result and quality for their nearest position
+        document.getElementById("answer").innerHTML = `The nearest station to your estimated position is in ${data.data.city.name}. The estimated AQI has a value of ${data.data.aqi}. The pollution rate is ${more}.`;
+      }
+    }
+    //optional further info
+    document.getElementById("answer").innerHTML += ` For further details, you can check out the reference website infos <a href="https://www.airnow.gov/aqi/aqi-basics/">here</a>.`;
+  });
 }
 
-function locate(){
-  let x = 10;
+//function to call fetch by text input
+function select(input){
+  //calls main function with input target given by button (text input by user)
+  locating(`city=${input}`);
 }
 
-    document.getElementById("butt1").addEventListener("click", function(){
-      select(document.getElementById("query").value);
-    });
-    document.getElementById("butt2").addEventListener("click", function(){
-      locate();
-    });
+//function to call fetch by geoloc input
+function geo(){
+  //tries to get current user position
+  const signal = new Promise((resolve, reject) => navigator.geolocation.getCurrentPosition(resolve, reject));
+  signal.then(pos => {
+    //store coordinates
+    const latit = pos.coordinates.latitude;
+    const longi = pos.coordinates.longitude;
+    //calls main function with input target given by button (stored coordinates above)
+    locating(`latit=${latit}&longi=${longi}`);
+  }
+}
+
+//click event handlers for buttons
+document.getElementById("butt1").addEventListener("click", function(){
+  //go and call main function with text input by user
+  select(document.getElementById("query").value);
+});
+document.getElementById("butt2").addEventListener("click", function(){
+  //go and find user coordinates and pass them to the main function
+  geo();
+});
