bb14617979772e38b18d955c14c4deb90ee75fc5
sunbrogilgamesh <87723910+sunbrogilgamesh@users.noreply.github.com>
2021-11-26 18:00:31 +0100

    update

--- src/index.js
+++ src/index.js
@@ -45,26 +45,26 @@ function locating(location){
       }
       //if search by geoloc
       else {
+        //calculate distance between 2 coordinates (45;9 -- 46;11)
+        const radius = 6371e3; // metres
+        const lat1 = data.data.city.geo[0];
+        const lon1 = data.data.city.geo[1];
+        const lat2 = location.substring(6, location.indexOf("&longi"));
+        const lon2 = location.substring(location.indexOf("&longi") + 7, location.length);
+        console.log(lat2 + " " + lon2);
+        const diam1 = lat1 * Math.PI/180;
+        const diam2 = lat2 * Math.PI/180;
+        const diff1 = (lat2 - lat1) * Math.PI/180;
+        const diff2 = (lon2 - lon1) * Math.PI/180;
+        const a = Math.sin(diff1/2) * Math.sin(diff1/2) +
+                  Math.cos(diam1) * Math.cos(diam2) *
+                  Math.sin(diff2/2) * Math.sin(diff2/2);
+                  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+                  const d = radius * c / 1000; // in kilometres
+        console.log(d + " kilometers");
         //tell user the result and quality for their nearest position
-        document.getElementById("answer").innerHTML = `The nearest station to your estimated position is in ${data.data.city.name}. The estimated AQI has a value of ${data.data.aqi}. The pollution rate is ${more}.`;
+        document.getElementById("answer").innerHTML = `La stazione di rilevamento più vicina a te è a ${data.data.city.name}. Si trova a circa ${d} chilometri.`;
       }
-      //calculate distance between 2 coordinates (45;9 -- 46;11)
-      const radius = 6371e3; // metres
-      const lat1 = data.data.city.geo[0];
-      const lon1 = data.data.city.geo[1];
-      const lat2 = location.substring(6, location.indexOf("&longi"));
-      const lon2 = location.substring(location.indexOf("&longi") + 7, location.length);
-      console.log(lat2 + " " + lon2);
-      const diam1 = lat1 * Math.PI/180;
-      const diam2 = lat2 * Math.PI/180;
-      const diff1 = (lat2 - lat1) * Math.PI/180;
-      const diff2 = (lon2 - lon1) * Math.PI/180;
-      const a = Math.sin(diff1/2) * Math.sin(diff1/2) +
-                Math.cos(diam1) * Math.cos(diam2) *
-                Math.sin(diff2/2) * Math.sin(diff2/2);
-      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-      const d = radius * c / 1000; // in kilometres
-      console.log(d + " kilometers");
     }
     //optional further info
     document.getElementById("answer").innerHTML += ` For further details, you can check out the reference website infos <a target="_blank" href="https://www.airnow.gov/aqi/aqi-basics/">here</a>.`;
