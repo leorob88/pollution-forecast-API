f1a040d41ac5118f4001e36c2a7e9153e0f5f7a3
sunbrogilgamesh <87723910+sunbrogilgamesh@users.noreply.github.com>
2021-12-30 17:33:10 +0100

    update

--- src/index.js
+++ src/index.js
@@ -111,8 +111,8 @@ function locating(location, searching){
         document.getElementById("question").innerHTML = "I couldn't use your position to find any stations for pollution detection. Do you want to try a name search?";
       }
       document.getElementById("page").style.visibility = "hidden";
-      document.getElementById("agree").style.visibility = "visible";
       document.getElementById("page").insertAfter(document.getElementById("agree"));
+      document.getElementById("agree").style.visibility = "visible";
     }
     //if response is found
     else {
