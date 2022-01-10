772e9225ee596b1fe3caefcb5664cf830b8f61a4
sunbrogilgamesh <87723910+sunbrogilgamesh@users.noreply.github.com>
2022-01-10 21:17:04 +0100

    update

--- src/index.js
+++ src/index.js
@@ -201,7 +201,7 @@ document.getElementById("button-keyword").addEventListener("click", function(){
 });
 document.getElementById("button-geoloc").addEventListener("click", function(){
   //go and call main function with user current position
-  getResult(`latit=${userLatitude}&longi=${userLongitude}`, this.value);
+  locating(`latit=${userLatitude}&longi=${userLongitude}`, this.value);
 });
 document.getElementById("keyword-results").addEventListener("change", selecting(results));
 
