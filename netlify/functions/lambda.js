ea0be6680e06793053313467365996635ee90699
sunbrogilgamesh <87723910+sunbrogilgamesh@users.noreply.github.com>
2022-01-11 13:14:05 +0100

    update

--- netlify/functions/lambda.js
+++ netlify/functions/lambda.js
@@ -6,17 +6,12 @@ exports.handler = async event => {
   let target = event.queryStringParameters;
   let data;
 
-  if (target.city){
-    const response = await fetch(`https://api.waqi.info/feed/${target.city}/?token=${API_KEY}`);
-    data = await response.json();
-  }else if (target.custom) {
-    const response = await fetch(`https://api.waqi.info/search/?keyword=${target.custom}&token=${API_KEY}`);
-    data = await response.json();
-  }else if (target.latit && target.longi) {
-    const response = await fetch(`https://api.waqi.info/feed/geo:${target.latit};${target.longi}/?token=${API_KEY}`);
-    data = await response.json();
-  }
+  let url = target.city != null ? `https://api.waqi.info/feed/${target.city}/?token=${API_KEY}` :
+          target.custom != null ? `https://api.waqi.info/search/?keyword=${target.custom}&token=${API_KEY}` :
+          (target.latit != null && target.longi != null) ? `https://api.waqi.info/feed/geo:${target.latit};${target.longi}/?token=${API_KEY}`;
 
+  const response = await fetch(url);
+  data = await response.json();
   return {
     statusCode: 200,
     body: JSON.stringify(data)
