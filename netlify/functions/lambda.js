9c2e476b3a88efe23f060128472edf033f2007ce
sunbrogilgamesh <87723910+sunbrogilgamesh@users.noreply.github.com>
2022-01-11 13:18:33 +0100

    update

--- netlify/functions/lambda.js
+++ netlify/functions/lambda.js
@@ -4,14 +4,19 @@ const API_KEY =  process.env.API_KEY;
 exports.handler = async event => {
 
   let target = event.queryStringParameters;
-  let data;
+  let data, url;
 
-  let url = target.city != null ? `https://api.waqi.info/feed/${target.city}/?token=${API_KEY}` :
-          target.custom != null ? `https://api.waqi.info/search/?keyword=${target.custom}&token=${API_KEY}` :
-          (target.latit != null && target.longi != null) ? `https://api.waqi.info/feed/geo:${target.latit};${target.longi}/?token=${API_KEY}`;
+  if (target.city){
+    url = `https://api.waqi.info/feed/${target.city}/?token=${API_KEY}`;
+  }else if (target.custom) {
+    url = `https://api.waqi.info/search/?keyword=${target.custom}&token=${API_KEY}`;
+  }else if (target.latit && target.longi){
+    url = `https://api.waqi.info/feed/geo:${target.latit};${target.longi}/?token=${API_KEY}`;
+  }
 
   const response = await fetch(url);
   data = await response.json();
+  
   return {
     statusCode: 200,
     body: JSON.stringify(data)
